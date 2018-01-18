bcde7edc961646c5133a293e0d2db92b9808bbae
Andrew <a.ermak@innopolis.ru>
2018-01-18 13:42:37 +0300

    File calling redused

--- src/Utils.kt
+++ src/Utils.kt
@@ -3,20 +3,20 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 object PatternsLoader {
-    // TODO reduce file call
-    private var path_str = "src/c_patterns.json"
-    private var file_content: String = String(Files.readAllBytes(Paths.get(path_str)))
-    private var json_object: JSONObject = JSONObject(file_content)
+    private val path_str = "src/c_patterns.json"
+    private val file_content: String = String(Files.readAllBytes(Paths.get(path_str)))
+    private val json_object: JSONObject = JSONObject(file_content)
+    private val dependencies: JSONObject = json_object.getJSONObject("dependencies")
+    private val standard_types: JSONObject = json_object.getJSONObject("standard_types")
+    private val patterns: JSONObject = json_object.getJSONObject("patterns")
 
 
     fun getPatternByType(type: PATTERN_TYPES): String {
-        return json_object.getJSONObject("patterns")
-                .getJSONObject(type.toString())
+        return patterns.getJSONObject(type.toString())
                 .getString("pattern")
     }
 
     fun getDependency(func_name: String): String {
-        val dependencies = json_object.getJSONObject("dependencies")
         for (key in dependencies.toMap().keys) {
             if (dependencies.getJSONArray(key).toList().contains(func_name))
                 return key
@@ -25,9 +25,8 @@ object PatternsLoader {
     }
 
     fun getType(slang_type: STD_TYPES): String {
-        val types = json_object.getJSONObject("standard_types")
-        if(types.has(slang_type.toString()))
-            return types.getString(slang_type.toString())
+        if(standard_types.has(slang_type.toString()))
+            return standard_types.getString(slang_type.toString())
         else
             return "ERROR_IN_TYPES"
     }
